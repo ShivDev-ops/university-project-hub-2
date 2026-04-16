9d15ffa79832f886e7aca8ae076e22bdd38c9b91
ShivDev-ops <shivendra22september@gmail.com>
2026-04-16 14:28:00 +0530

    Auth commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,14 @@ import { authOptions } from '@/lib/auth'
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    verified:         boolean
+    profile_complete: boolean
+    is_admin:         boolean
+    is_suspended:     boolean
+    dbUserId:         string
+    hasCredentials:   boolean
+  }
+}
